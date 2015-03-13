458b921de6c469d501b3b38d90d961698a71c82e
David McMackins II <contact@mcmackins.org>
2015-03-13 15:02:45 -0500

    Complete rewrite (and relicense) to allow for multiple remote APIs

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-
 setup(
     name = 'coinfetch',
-    version = '3.0.0',
-    scripts = ['coinfetch'],
+    version = '4.0.0',
+    scripts = ['coinfetch', 'bterfetch'],
+    py_modules = ['coinfetchapi'],
 
     install_requires = ['requests'],
 
@@ -15,7 +15,7 @@ setup(
     author = 'Delwink, LLC',
     author_email = 'support@delwink.com',
     description = 'Cryptocurrency price converter',
-    license = 'MIT',
+    license = 'AGPLv3',
     keywords = 'coin fetch coinfetch ticker bitcoin cryptocurrency money',
     url = 'http://delwink.com/software/coinfetch.html'
 )
